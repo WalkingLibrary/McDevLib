d80e964263554f2aac87c28e8d47a1eb9032a3f1
BuildTools <jumboofawarrior@gmail.com>
2019-12-21 06:49:39 -0700

    Adjustments Dec 21, 2019 master #3 -jit Pack Build Fix
    Signed-off-by: BuildTools <jumboofawarrior@gmail.com>

--- src/main/java/com/mixin/MixinNetworkManager.java
+++ src/main/java/com/mixin/MixinNetworkManager.java
@@ -1,6 +1,7 @@
 package com.mixin;
 
 
+
 import com.jumbodinosaurs.mcdevlib.OneDotTwelve.events.PacketEvent;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.util.concurrent.GenericFutureListener;
@@ -18,47 +19,40 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import javax.annotation.Nullable;
 import java.util.concurrent.Future;
 
-@Mixin(NetworkManager.class) public abstract class MixinNetworkManager extends SimpleChannelInboundHandler<Packet<?>>
-{
+@Mixin(NetworkManager.class)
+public abstract class MixinNetworkManager extends SimpleChannelInboundHandler <Packet<? >> {
     private boolean sendPackets = true;
     @Shadow
-    protected abstract void dispatchPacket(final Packet<?> inPacket,
-                                           @Nullable final GenericFutureListener<? extends Future<? super Void>>[] futureListeners);
+    protected abstract void dispatchPacket(final Packet<?> inPacket, @Nullable final GenericFutureListener<? extends Future<? super Void>>[] futureListeners);
     
     @Redirect(method = "channelRead0", at = @At(value = "INVOKE", target = "net/minecraft/network/Packet.processPacket(Lnet/minecraft/network/INetHandler;)V"))
-    private void channelRead0$processPacket(Packet<?> packetIn, INetHandler handler)
-    {
+    private void channelRead0$processPacket(Packet<?> packetIn, INetHandler handler) {
         PacketEvent event = new PacketEvent.Receive(packetIn);
         MinecraftForge.EVENT_BUS.post(event);
-        if(event.isCanceled())
+        if (event.isCanceled())
             return;
         
         ((Packet<INetHandler>) event.getPacket()).processPacket(handler);
     }
     
     @Redirect(method = "sendPacket", at = @At(value = "INVOKE", target = "net/minecraft/network/NetworkManager.dispatchPacket(Lnet/minecraft/network/Packet;[Lio/netty/util/concurrent/GenericFutureListener;)V"))
-    private void sendPacket$dispatchPacket(NetworkManager networkManager,
-                                           Packet<?> packetIn,
-                                           @Nullable final GenericFutureListener<? extends Future<? super Void>>[] futureListeners)
-    {
+    private void sendPacket$dispatchPacket(NetworkManager networkManager, Packet<?> packetIn, @Nullable final GenericFutureListener<? extends Future<?super Void>>[] futureListeners) {
         PacketEvent event = new PacketEvent.Send(packetIn);
         MinecraftForge.EVENT_BUS.post(event);
-        if(event.isCanceled())
+        if (event.isCanceled())
             return;
         
-        this.dispatchPacket(event.getPacket(), futureListeners);
+        this.dispatchPacket(event.getPacket(), (GenericFutureListener[])futureListeners);
     }
     
     @Redirect(method = "sendPacket", at = @At(value = "INVOKE", target = "net/minecraft/network/NetworkManager.isChannelOpen()Z"))
-    private boolean sendPacket$isChannelOpen(NetworkManager networkManager)
-    {
+    private boolean sendPacket$isChannelOpen(NetworkManager networkManager) {
         return this.sendPackets && networkManager.isChannelOpen();
     }
     
     @Inject(method = "flushOutboundQueue", at = @At("HEAD"), cancellable = true)
-    private void flushOutboundQueue(CallbackInfo ci)
-    {
-        if(!sendPackets)
+    private void flushOutboundQueue(CallbackInfo ci) {
+        if (!sendPackets)
             ci.cancel();
     }
 }
