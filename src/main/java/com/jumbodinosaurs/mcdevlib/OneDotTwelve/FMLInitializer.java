7d987d6f951c22c1f81f9305276362125403d011
Caleb <43940765+walkinglibrary@users.noreply.github.com>
2019-12-22 08:07:07 -0700

    Adjustments Dec 21, 2019 master #9 - Menu Test
    Signed-off-by: BuildTools <jumboofawarrior@gmail.com>

---
+++ src/main/java/com/jumbodinosaurs/mcdevlib/OneDotTwelve/FMLInitializer.java
@@ -0,0 +1,13 @@
+package com.jumbodinosaurs.mcdevlib.OneDotTwelve;
+
+
+import net.minecraftforge.fml.common.Mod;
+
+
+@Mod(useMetadata = true, modid = FMLInitializer.modid, version = FMLInitializer.version) public class FMLInitializer
+{
+    
+    public static final String modid = "mcdevlib";
+    public static final String version = "0.0.1";
+    
+}
