167602f55b3214b963f6952c2dc706fc1c558547
Caleb <43940765+walkinglibrary@users.noreply.github.com>
2019-12-21 23:36:15 -0700

    Adjustments Dec 21, 2019 master #7 - FML Plugin Change
    Signed-off-by: BuildTools <jumboofawarrior@gmail.com>

--- src/main/java/com/mixin/Core.java
+++ src/main/java/com/mixin/Core.java
@@ -1,16 +1,49 @@
 package com.mixin;
 
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.MixinEnvironment;
 import org.spongepowered.asm.mixin.Mixins;
 
-public class Core
+import java.util.Map;
+
+public class Core implements IFMLLoadingPlugin
 {
-    public Core()
     
+    
+    public Core()
     {
         MixinBootstrap.init();
         Mixins.addConfiguration("mixinsDev.json");
         MixinEnvironment.getDefaultEnvironment().setObfuscationContext("searge");
+    }
+    
+    @Override
+    public String[] getASMTransformerClass()
+    {
+        return new String[0];
+    }
+    
+    @Override
+    public String getModContainerClass()
+    {
+        return null;
+    }
+    
+    @Override
+    public String getSetupClass()
+    {
+        return null;
+    }
+    
+    @Override
+    public void injectData(Map<String, Object> data)
+    {
+    }
+    
+    @Override
+    public String getAccessTransformerClass()
+    {
+        return null;
     }
 }
