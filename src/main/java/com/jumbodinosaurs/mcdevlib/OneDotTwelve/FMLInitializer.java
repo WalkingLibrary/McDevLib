94fc452790c66f7983d3d891468322860aff8ef8
Caleb <43940765+walkinglibrary@users.noreply.github.com>
2019-12-22 22:57:07 -0700

    Adjustments Dec 21, 2019 master #10 - Menu Test
    Signed-off-by: BuildTools <jumboofawarrior@gmail.com>

--- src/main/java/com/jumbodinosaurs/mcdevlib/OneDotTwelve/FMLInitializer.java
+++ src/main/java/com/jumbodinosaurs/mcdevlib/OneDotTwelve/FMLInitializer.java
@@ -1,7 +1,10 @@
 package com.jumbodinosaurs.mcdevlib.OneDotTwelve;
 
 
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 
 
 @Mod(useMetadata = true, modid = FMLInitializer.modid, version = FMLInitializer.version) public class FMLInitializer
@@ -9,5 +12,18 @@ import net.minecraftforge.fml.common.Mod;
     
     public static final String modid = "mcdevlib";
     public static final String version = "0.0.1";
+    
+    @Mod.EventHandler
+    public void preInit(FMLInitializationEvent event)
+    {
+    
+    }
+    
+    
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
     
 }
