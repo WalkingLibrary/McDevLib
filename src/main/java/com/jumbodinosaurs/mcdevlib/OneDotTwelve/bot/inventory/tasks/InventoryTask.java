27a7c4b4b043436ff6d72a188887682be5fc9d53
Caleb <43940765+walkinglibrary@users.noreply.github.com>
2020-01-29 03:36:02 -0700

    Adjustments - removed InventoryTask
    Signed-off-by: BuildTools <jumboofawarrior@gmail.com>

--- src/main/java/com/jumbodinosaurs/mcdevlib/OneDotTwelve/bot/inventory/tasks/InventoryTask.java
+++
@@ -1,9 +0,0 @@
-package com.jumbodinosaurs.mcdevlib.OneDotTwelve.bot.inventory.tasks;
-
-
-import net.minecraft.inventory.Container;
-
-public abstract class InventoryTask
-{
-    public abstract boolean execute(Container container);
-}
